0ba4f807eaaa4ec6af92a129240ad08c3591f0b3
jayabalan2008 <jayabalanelangovan156@gmail.com>
2026-04-21 15:06:41 +0530

    initial commit

---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class TicTacToe {
+    static char[][] board =  new char[3][3];
+
+    public static void main(String[] args){
+        intializeBoard();
+        printBoard();
+    }
+}
